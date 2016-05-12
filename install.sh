780caeda9fd4a55e4049d1d3066a2b3f7c0a6cf9
Sagar Desai <sagar.desai@uoit.net>
2016-05-12 12:53:00 +0000

    Install script now install pip for nvim python modules

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Install NeoVim
 sudo apt-get install software-properties-common
+sudo apt-get install python-dev python-pip python3-dev python3-pip
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
 sudo apt-get install neovim
@@ -22,6 +23,7 @@ ln -s ~/.vim $XDG_CONFIG_HOME/nvim
 ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
 
 # Build youcompleteme plugin
+sudo apt-get install build-essential cmake
 cd ~/.vim/bundle/youcompleteme
 ./install.py --tern-completer
 
