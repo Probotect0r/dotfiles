4ee686fb7ce55b77ea67a7349cfc98056ded58c7
Sagar Desai <sagar.desai@uoit.net>
2016-05-12 15:19:58 +0000

    Fixed errors in install script

--- install.sh
+++ install.sh
@@ -5,17 +5,25 @@ cd ~
 # Install Antigen
 curl -L https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh > antigen.zsh
 
+# Install tmux 2.0
+sudo add-apt-repository ppa:pi-rho/dev
+sudo apt-get update
+sudo apt-get install tmux
+
 # Install tmux plugin manager
-clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Install Vundle plugin manager
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Install NeoVim
-sudo apt-get install software-properties-common
-sudo apt-get install python-dev python-pip python3-dev python3-pip
+sudo  apt-get install -y software-properties-common
+sudo apt-get install -y python-dev python-pip python3-dev python3-pip
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
-sudo apt-get install neovim
-sudo pip2 install neovi
-sudo pip3 install neovim
+sudo apt-get install -y neovim
+sudo pip2 install -y neovi
+sudo pip3 install -y neovim
 vim +PluginInstall +qall
 
 mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
