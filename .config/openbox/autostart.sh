a6ca68511a1948663575c137ed7aa79b9fadab25
sagar desai <sagarmonkeyboy@gmail.com>
2016-09-15 00:32:12 -0400

    Added font to openbox autostart command.

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -2,5 +2,5 @@ xrandr --output DVI-I-1 --primary --right-of HDMI-0 --output DVI-D-0 --right-of 
 compton -f &
 xrdb ~/.Xresources
 feh --bg-scale ~/Pictures/wallpaper.jpg
-~/bar_feeder.sh | lemonbar -p &
+~/bar_feeder.sh | lemonbar -p & -font Iosevka:size=11 -font FontAwesome
 stalonetray --dockapp-mode simple -p &
