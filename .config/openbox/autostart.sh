8fa8b45d430d48e3535a9e41f665f206d3138f61
Sagar Desai <sagar.desai@uoit.net>
2016-09-17 02:57:18 +0000

    Updated openbox autostart.

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -1,6 +1,5 @@
+admiral | lemonbar -p & -font Iosevka:size=11 -font FontAwesome
 xrandr --output DVI-I-1 --primary --right-of HDMI-0 --output DVI-D-0 --right-of DVI-I-1
-compton -f &
 xrdb ~/.Xresources
 feh --bg-scale ~/Pictures/wallpaper.jpg
-~/bar_feeder.sh | lemonbar -p & -font Iosevka:size=11 -font FontAwesome
-stalonetray --dockapp-mode simple -p &
+gksudo NetworkManager
