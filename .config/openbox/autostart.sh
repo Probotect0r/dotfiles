e3bd3b10f2804e82dc87d92fdd81e7c6a8508a9d
Sagar Desai <sagar.desai@uoit.net>
2016-09-18 03:06:37 +0000

    Updated font size in lemonbar.

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -1,4 +1,4 @@
-admiral | lemonbar -p & -font Iosevka:size=11 -font FontAwesome
+admiral | lemonbar -p -font Iosevka:size=10 -font FontAwesome &
 xrandr --output DVI-I-1 --primary --right-of HDMI-0 --output DVI-D-0 --right-of DVI-I-1
 xrdb ~/.Xresources
 feh --bg-scale ~/Pictures/wallpaper.jpg
