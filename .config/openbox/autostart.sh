0544499d292c363885ffbd250852b3526170fc7b
Sagar Desai <sagar.desai@uoit.net>
2016-09-19 03:17:04 +0000

    Fixed lemonbar statemnt in autostart file.

--- .config/openbox/autostart.sh
+++ .config/openbox/autostart.sh
@@ -1,5 +1,4 @@
-admiral | lemonbar -p -font Iosevka:size=10 -font FontAwesome &
-xrandr --output DVI-I-1 --primary --right-of HDMI-0 --output DVI-D-0 --right-of DVI-I-1
-xrdb ~/.Xresources
 feh --bg-scale ~/Pictures/wallpaper.jpg
 gksudo NetworkManager
+xrdb ~/.Xresources
+admiral | lemonbar -f Iosevka:size=10 -f FontAwesome:size=10 &
