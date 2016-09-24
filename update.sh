b5c6629307bb1ade7382487d2539433f66b027e0
Sagar Desai <sagar.desai@uoit.net>
2016-09-24 18:16:49 -0400

    Added openbox themes folder.

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cp -r ~/.vimrc ~/.tmux.conf ~/.zshrc ~/.Xresources ~/.vim/colors/generic.vim ./
 cp -r ~/.config/i3 ~/.config/terminator ~/.config/openbox ~/.config/i3status ~/.config/admiral.d ./.config/
-cp -r ~/.fonts ./
+cp -r ~/.fonts ~/.themes ./
