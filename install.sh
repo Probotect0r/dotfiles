09705e32ba08d66800b289da9bbc5e9d50d73eee
Sagar Desai <sagar.desai@uoit.net>
2016-10-14 23:52:09 -0400

    Cleaned up the forlder.

--- install.sh
+++ install.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 cp -r ./.vimrc ./.tmux.conf ./.Xresources ./.zshrc ~/
-cp -r ./.config/* ~/.config/
-cp -r ./generic.vim ~/.vim/colors/
-cp ./bar_feeder.sh ~/
-cp -r ./.fonts ./.themes ~/
+cp -r ./.fonts ./.themes ./.vim ./.config ~/
