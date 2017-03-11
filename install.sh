a433e2d9793f1b3e32b66740d17794604a044ccc
Sagar Desai <sagar.desai@uoit.net>
2017-03-11 17:20:57 -0500

    Added aliases file to install script.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp -r ./.vimrc ./.tmux.conf ./.Xresources ./.zshrc ~/
+cp -r ./.vimrc ./.tmux.conf ./.Xresources ./.zshrc ./.aliases.sh ~/
 cp -r ./generic.vim ~/.vim/colors/
 cp -r ./.fonts ./.themes ./.vim ./.config ~/
 cp ./wallpaper.png ~/Pictures/
