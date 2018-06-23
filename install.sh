7f46b1b0881ec1cb00a9af4c8da2f6945b5d1123
Sagar Desai <sagar.desai@rbc.com>
2018-06-23 02:16:08 +0000

    Added creation of Pictures directory to install script

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cp -r ./.vimrc ./.tmux.conf ./.zshrc  ./.profile ./.aliases.sh ./.Xresources ~/
 cp -r ./.fonts ./.vim ./.config ~/
+
+mkdir -p ~/Pictures
 cp ./wallpaper.png ~/Pictures/
