c8bada130c939cae87457d6a48fcd6aa376249e2
Sagar Desai <Probotect0r@users.noreply.github.com>
2022-06-27 11:29:20 -0400

    added aws-params function

--- shell/.aliases.sh
+++ shell/.aliases.sh
@@ -13,9 +13,16 @@ alias vim="nvim"
 # Docker
 alias dr="docker run --rm -it"
 
-# Interactive aws connect
+# Interactive aws connect to ec2 instances using SSM
 aws-connect() {
  instance=$(aws ec2 describe-instances | jq '.Reservations| .[] | .Instances | .[] | "\(.Tags | .[] | select(.Key == "Name") | .Value), \(.InstanceId)"' | fzf | sed 's/"//g' | awk -F ", " '{print $2}')
  echo "$instance"
  aws ssm start-session --target "$instance"
+}
+
+aws-params() {
+  param=$(aws ssm describe-parameters | jq '.Parameters | .[] | "\(.Name) \(.Description)"' | fzf | sed 's/"//g' | awk -F " " '{print $1}')
+  echo "$param"
+  aws ssm get-parameter --name $param --with-decryption | jq '.Parameter | .Value' | xargs printf
+  echo
 }
